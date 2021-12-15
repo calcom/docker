6c7ebb345a8fd88283616c8f753f8a6fef6dcdad
Flemming Petersen <mail@flemming-petersen.de>
2021-12-15 16:01:30 +0100

    Make use of internal docker networks unifi database name

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -x
 
-/app/scripts/wait-for-it.sh ${DATABASE_HOST} -- echo "db is up"
+/app/scripts/wait-for-it.sh ${DATABASE_HOST} -- echo "database is up"
 npx prisma migrate deploy
 yarn start
