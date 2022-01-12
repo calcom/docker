0910dd0349333b2b36a91c950bd0efedea60aaac
Colin Griffin <colin@krum.io>
2022-01-12 12:48:15 -0500

    add script changes and docker-compose configuration for local development

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,14 @@
-#!/bin/sh
+#!/bin/bash
 set -x
+
+# # Set environment variables
+# echo NEXT_PUBLIC_APP_URL $NEXT_PUBLIC_APP_URL
+# find \( -type d -name .git -prune \) -o -type f -print0 | xargs -0 sed -i "s#$NEXT_PUBLIC_APP_URL#$NEXT_PUBLIC_APP_URL_SUBSTITUTE#g"
 
 /app/scripts/wait-for-it.sh ${DATABASE_HOST} -- echo "database is up"
 npx prisma migrate deploy
-yarn start
+if [[ $NODE_ENV == "development" ]]; then
+    yarn dev
+else
+    yarn start
+fi
