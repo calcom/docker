a08ccae05e4effcfba988d49eb2e1c852989e48c
Colin Griffin <colin@krum.io>
2022-06-01 16:18:49 -0400

    add app seed script to startup

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,4 +3,5 @@ set -x
 
 scripts/wait-for-it.sh ${DATABASE_HOST} -- echo "database is up"
 npx prisma migrate deploy --schema /calcom/packages/prisma/schema.prisma
+npx ts-node --transpile-only /calcom/packages/prisma/seed-app-store.ts
 yarn start
