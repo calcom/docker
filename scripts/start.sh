46a0e729dba6516d9e66abaa5ccdb2bfdcb8bcc5
Connery Noble <connery@qwerti.ca>
2022-09-08 17:05:31 -0700

    feat(frontend): use runtime NEXT_PUBLIC_WEBAPP_URL to replace build-time frontend value

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -x
 
+scripts/replace-placeholders.sh
 scripts/wait-for-it.sh ${DATABASE_HOST} -- echo "database is up"
 npx prisma migrate deploy --schema /calcom/packages/prisma/schema.prisma
 npx ts-node --transpile-only /calcom/packages/prisma/seed-app-store.ts
