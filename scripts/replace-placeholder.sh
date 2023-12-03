32b32be14e9256586396700f36795933789f4ede
Gabriela Georgieva <georgievaga@dhcp-vl2051-1227.eduroam.cornell.edu>
2023-12-03 04:06:42 -0500

    Changing build-time env variables to run-time only

--- scripts/replace-placeholder.sh
+++ scripts/replace-placeholder.sh
@@ -1,16 +1,22 @@
-FROM=$1
-TO=$2
-
-if [ "${FROM}" = "${TO}" ]; then
-    echo "Nothing to replace, the value is already set to ${TO}."
-
-    exit 0
-fi
-
-# Only peform action if $FROM and $TO are different.
-echo "Replacing all statically built instances of $FROM with $TO."
-
-find apps/web/.next/ apps/web/public -type f |
-while read file; do
-    sed -i "s|$FROM|$TO|g" "$file"
+#!/bin/sh
+ 
+# List of variables to replace
+VARIABLES="NEXT_PUBLIC_WEBAPP_URL NEXT_PUBLIC_LICENSE_CONSENT CALCOM_TELEMETRY_DISABLED NEXTAUTH_SECRET CALENDSO_ENCRYPTION_KEY DATABASE_URL"
+ 
+for VAR in $VARIABLES; do
+    # Retrieve the current value of the variable
+    TO_VALUE=$(eval echo \$$VAR)
+ 
+    # Only proceed if the variable is set and not empty
+    if [ ! -z "$TO_VALUE" ]; then
+        FROM_PLACEHOLDER="PLACEHOLDER_${VAR}"
+        echo "Replacing $FROM_PLACEHOLDER with $TO_VALUE"
+ 
+        find apps/web/.next/ apps/web/public -type f |
+        while read file; do
+            sed -i "s|$FROM_PLACEHOLDER|$TO_VALUE|g" "$file"
+        done
+    else
+        echo "Variable $VAR is not set. Skipping replacement for $VAR."
+    fi
 done
