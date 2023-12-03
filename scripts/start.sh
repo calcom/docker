fcad77d4e68de61e01c24a5a8ebfb41c1f502d84
Adrián Darío Hidalgo Flores <ahidalgo@idealizer.mx>
2023-12-03 16:03:07 -0600

    fix: recovers replace-placeholder script

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Replace http://NEXT_PUBLIC_WEBAPP_URL_PLACEHOLDER
+NEXT_PUBLIC_WEBAPP_URL=$(grep "^NEXT_PUBLIC_WEBAPP_URL=" .env | awk -F '=' '{print $2}')
+sh scripts/replace-placeholder.sh "http://NEXT_PUBLIC_WEBAPP_URL_PLACEHOLDER" $NEXT_PUBLIC_WEBAPP_URL
+
 # Database deploy
 yarn workspace @calcom/prisma db-deploy
 yarn workspace @calcom/prisma seed-app-store
