67aee002b238365eabb5a89cb50883e84583c323
Keith Williams <keithwillcode@gmail.com>
2024-02-28 20:13:03 -0300

    fix: Broken Docker builds

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,4 +8,5 @@ scripts/replace-placeholder.sh "$BUILT_NEXT_PUBLIC_WEBAPP_URL" "$NEXT_PUBLIC_WEB
 scripts/wait-for-it.sh ${DATABASE_HOST} -- echo "database is up"
 npx prisma migrate deploy --schema /calcom/packages/prisma/schema.prisma
 npx ts-node --transpile-only /calcom/packages/prisma/seed-app-store.ts
+yarn install
 yarn start
